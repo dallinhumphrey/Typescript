32356a9c885237ef354fe0db5ce03032a6e77f04
dallinhumphrey <dallinhumphrey@gmail.com>
2020-07-02 10:42:18 -0600

    variable errors

--- 006_variables.js
+++ 006_variables.js
@@ -1,17 +1,14 @@
 // var fullName: string = "Dallin Humphrey";
 // let paidAccount: boolean = true;
 // const versionNumber: number = 1.3;
-
 // fullName = "Kortney Humphrey";
 // paidAccount = false;
-
 // console.log(fullName);
 // console.log(paidAccount);
 // console.log(versionNumber);
-
 function printName(f, l) {
-  var greeting: string = "Hi there, ";
-  console.log(greeting + f + " " + l);
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
 }
-
 printName("Dallin", "Hudgends");
+//# sourceMappingURL=006_variables.js.map
