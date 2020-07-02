4283868b38ee49f25726759e616c15956e6afeb4
dallinhumphrey <dallinhumphrey@gmail.com>
2020-07-02 12:08:21 -0600

    forgot to save

--- 007_types.ts
+++ 007_types.ts
@@ -1,15 +1,32 @@
 // Boolean
 let paidAccount: boolean = true;
+
 // Number
+let age: number = 33;
+var taxRate: number = 7.5;
 
 // String
+var fullName: string = "Dallin Humphrey";
 
 // Array
+var ages: number[] = [33, 28, 11];
 
 // Tuple
+let player: [number, string];
+player = [3, "Humphrey"];
 
-// Enum
+// Enum AWESOME!!!!
+enum ApprovalStatus {
+  Approved,
+  Pending,
+  Rejected,
+}
+let job: ApprovalStatus = ApprovalStatus.Pending;
 
-// Any
+// Any works only with any specifically.
+var apiData: any[] = [123, "Dallin", false];
 
-// Void
+// Void use when using actions but no value
+function printOut(msg: string): void {
+  console.log(msg);
+}
