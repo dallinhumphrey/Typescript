e9a27f3d8ced693a8b8c640b2056b1866cfa96fd
dallinhumphrey <dallinhumphrey@gmail.com>
2020-07-07 09:24:58 -0600

    conditionals

---
+++ 011_conditionals.js
@@ -0,0 +1,11 @@
+var password = "zxcvzxcv";
+if (password == "asdfasdf") {
+    console.log("Yes, this is the password");
+}
+else if (password == "zxcvzxcv") {
+    console.log("dring your Ovaltine");
+}
+else {
+    console.log("Wrong Password");
+}
+//# sourceMappingURL=011_conditionals.js.map
---
+++ 011_conditionals.ts
@@ -0,0 +1,9 @@
+let password: string = "zxcvzxcv";
+
+if (password == "asdfasdf") {
+  console.log("Yes, this is the password");
+} else if (password == "zxcvzxcv") {
+  console.log("dring your Ovaltine");
+} else {
+  console.log("Wrong Password");
+}
