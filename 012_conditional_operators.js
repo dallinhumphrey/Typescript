9e1c0782b5ac377488afcbba754efc7d4b60856a
dallinhumphrey <dallinhumphrey@gmail.com>
2020-07-07 09:40:48 -0600

    conditional operators

---
+++ 012_conditional_operators.js
@@ -0,0 +1,39 @@
+var x = 100;
+// if (x == 200) {
+//   console.log("Condition passed");
+// } else {
+//   console.log("Condition failed");
+// }
+// if (x === 100) {
+//   console.log("Condition passed");
+// } else {
+//   console.log("Condition failed");
+// }
+// if (x != 100) {
+//   console.log("Condition passed");
+// } else {
+//   console.log("Condition failed");
+// }
+// if (x > 100) {
+//   console.log("Condition passed");
+// } else {
+//   console.log("Condition failed");
+// }
+// if (x >= 100) {
+//   console.log("Condition passed");
+// } else {
+//   console.log("Condition failed");
+// }
+if (x <= 100) {
+    console.log("Condition passed");
+}
+else {
+    console.log("Condition failed");
+}
+if (x < 100) {
+    console.log("Condition passed");
+}
+else {
+    console.log("Condition failed");
+}
+//# sourceMappingURL=012_conditional_operators.js.map
---
+++ 012_conditional_operators.ts
@@ -0,0 +1,43 @@
+let x: number = 100;
+
+if (x == 200) {
+  console.log("Condition passed");
+} else {
+  console.log("Condition failed");
+}
+
+if (x === 100) {
+  console.log("Condition passed");
+} else {
+  console.log("Condition failed");
+}
+
+if (x != 100) {
+  console.log("Condition passed");
+} else {
+  console.log("Condition failed");
+}
+
+if (x > 100) {
+  console.log("Condition passed");
+} else {
+  console.log("Condition failed");
+}
+
+if (x >= 100) {
+  console.log("Condition passed");
+} else {
+  console.log("Condition failed");
+}
+
+if (x <= 100) {
+  console.log("Condition passed");
+} else {
+  console.log("Condition failed");
+}
+
+if (x < 100) {
+  console.log("Condition passed");
+} else {
+  console.log("Condition failed");
+}
