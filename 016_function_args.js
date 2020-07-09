209570a9e8d179b0708776f7eea690b94a8ae317
dallinhumphrey <dallinhumphrey@gmail.com>
2020-07-09 10:06:41 -0600

    rest options

--- 016_function_args.js
+++ 016_function_args.js
@@ -1,12 +1,23 @@
-function printAddress(street, streetTwo, state) {
-    if (state === void 0) { state = "AZ"; }
-    console.log(street);
-    if (streetTwo) {
-        console.log(streetTwo);
+// function printAddress(street: string, streetTwo?: string, state = "AZ") {
+//   console.log(street);
+//   if (streetTwo) {
+//     console.log(streetTwo);
+//   }
+//   console.log(state);
+// }
+// printAddress("123 Any St");
+// printAddress("123 Any St", "suit 540");
+// printAddress("123 Any St", "Suit 560", "Utah");
+function lineupCard(team) {
+    var players = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        players[_i - 1] = arguments[_i];
     }
-    console.log(state);
+    console.log("Team: " + team);
+    for (var _a = 0, players_1 = players; _a < players_1.length; _a++) {
+        var player = players_1[_a];
+        console.log(player);
+    }
 }
-printAddress("123 Any St");
-printAddress("123 Any St", "suit 540");
-printAddress("123 Any St", "Suit 560", "Utah");
+lineupCard("Dragonlance", "Raistlin", "Caramon", "Takisis");
 //# sourceMappingURL=016_function_args.js.map
--- 016_function_args.ts
+++ 016_function_args.ts
@@ -1,11 +1,20 @@
-function printAddress(street: string, streetTwo?: string, state = "AZ") {
-  console.log(street);
-  if (streetTwo) {
-    console.log(streetTwo);
+// function printAddress(street: string, streetTwo?: string, state = "AZ") {
+//   console.log(street);
+//   if (streetTwo) {
+//     console.log(streetTwo);
+//   }
+//   console.log(state);
+// }
+
+// printAddress("123 Any St");
+// printAddress("123 Any St", "suit 540");
+// printAddress("123 Any St", "Suit 560", "Utah");
+
+function lineupCard(team: string, ...players: string[]) {
+  console.log("Team: " + team);
+  for (let player of players) {
+    console.log(player);
   }
-  console.log(state);
 }
 
-printAddress("123 Any St");
-printAddress("123 Any St", "suit 540");
-printAddress("123 Any St", "Suit 560", "Utah");
+lineupCard("Dragonlance", "Raistlin", "Caramon", "Takisis");
