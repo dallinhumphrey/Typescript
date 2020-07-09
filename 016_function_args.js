cdbc579eadf1ff776fee05bea4805d8a7ccbd27b
dallinhumphrey <dallinhumphrey@gmail.com>
2020-07-09 10:01:14 -0600

    more options

--- 016_function_args.js
+++ 016_function_args.js
@@ -1,9 +1,12 @@
-function printAddress(street, streetTwo) {
+function printAddress(street, streetTwo, state) {
+    if (state === void 0) { state = "AZ"; }
     console.log(street);
     if (streetTwo) {
         console.log(streetTwo);
     }
+    console.log(state);
 }
 printAddress("123 Any St");
 printAddress("123 Any St", "suit 540");
+printAddress("123 Any St", "Suit 560", "Utah");
 //# sourceMappingURL=016_function_args.js.map
--- 016_function_args.ts
+++ 016_function_args.ts
@@ -1,9 +1,11 @@
-function printAddress(street: string, streetTwo?: string) {
+function printAddress(street: string, streetTwo?: string, state = "AZ") {
   console.log(street);
   if (streetTwo) {
     console.log(streetTwo);
   }
+  console.log(state);
 }
 
 printAddress("123 Any St");
 printAddress("123 Any St", "suit 540");
+printAddress("123 Any St", "Suit 560", "Utah");
