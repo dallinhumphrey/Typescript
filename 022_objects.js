4eeeb9800a1a9e2c0c69673893331064bc7e3104
dallinhumphrey <dallinhumphrey@gmail.com>
2020-07-28 10:14:23 -0600

    objects in ts

--- 022_objects.js
+++ 022_objects.js
@@ -4,10 +4,14 @@ var realUser = {
     lastName: "Humphrey",
     sayHi: function () {
         return "Hey there!";
+    },
+    sayBye: function () {
+        return "Bye bye now!";
     }
 };
 console.log(realUser.email);
 console.log(realUser.firstName);
 console.log(realUser.lastName);
 console.log(realUser.sayHi());
+console.log(realUser.sayBye());
 //# sourceMappingURL=022_objects.js.map
--- 022_objects.ts
+++ 022_objects.ts
@@ -5,9 +5,13 @@ var realUser = {
   sayHi() {
     return "Hey there!";
   },
+  sayBye() {
+    return "Bye bye now!";
+  },
 };
 
 console.log(realUser.email);
 console.log(realUser.firstName);
 console.log(realUser.lastName);
 console.log(realUser.sayHi());
+console.log(realUser.sayBye());
